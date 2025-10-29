8efb161cf2ad7ceb479d7025c58b0fc8beb51d1f
Leapheng Keam <leaphengkeam@gmail.com>
2025-10-29 10:21:35 -0400

    create a switch case statement for a main menu to improve functionality

--- Assignments01/Assignment01.cs
+++ Assignments01/Assignment01.cs
@@ -4,29 +4,53 @@
 
 using Assignments01;
 
-Console.WriteLine("****HACKER NAME GENERATOR!****");
-Console.WriteLine("Please enter your favorite color: ");
-String color = Console.ReadLine();
-Console.WriteLine("Please enter your astrology sign: ");
-String sign = Console.ReadLine();
-Console.WriteLine("Please enter your street address number: ");
-String adNum = Console.ReadLine();
-Console.WriteLine($"Woohoo! Your hacker name is {color}{sign}{adNum}");
-Console.WriteLine(new string('*', 100)+ "\n");
+int choice = -1;
 
-// Practice number sizes and range
-// Call all methods from UnderstandingType class
-UnderstandingType.PrintHeader();
-// Call PrintInfo() to print all the data
-UnderstandingType.PrintInfo();
-Console.WriteLine(new string('*', 100)+ "\n");
+// Create a main menu with Switch Case statement
+while (choice != 0)
+{
+    Console.WriteLine("\t***WELCOME TO C# PRACTICE***");
+    Console.WriteLine("1. Hacker Name Generator\n" +
+                      "2. Practice Number Sizes and Range\n" +
+                      "3. Convert Centuries into Days,Years, Hours...\n" +
+                      "0. Exit");
+    Console.WriteLine("Please enter a number choice from the menu:");
+    choice = int.Parse(Console.ReadLine());
+    
+    switch (choice)
+    {
+        case 1: //Playing with console app
+            Console.WriteLine("\t****HACKER NAME GENERATOR!****");
+            Console.WriteLine("Please enter your favorite color: ");
+            String color = Console.ReadLine();
+            Console.WriteLine("Please enter your astrology sign: ");
+            String sign = Console.ReadLine();
+            Console.WriteLine("Please enter your street address number: ");
+            String adNum = Console.ReadLine();
+            Console.WriteLine($"Woohoo! Your hacker name is {color}{sign}{adNum}");
+            Console.WriteLine(new string('*', 100)+ "\n");
+        break;
+        case 2: // Practice number sizes and range
+            
+            // Call all methods from UnderstandingType class
+            UnderstandingType.PrintHeader();
+            // Call PrintInfo() to print all the data
+            UnderstandingType.PrintInfo();
+            Console.WriteLine(new string('*', 100)+ "\n"); 
+            break;
+        case 3: // Centuries Converter
+            Console.WriteLine("Please enter a number of centuries:");
+            int centuries = int.Parse(Console.ReadLine());
+            // Call calCenturies() to do calculation from UnderstandingType class
+            UnderstandingType.calCenturies(centuries);
+            Console.WriteLine(new string('*', 100)+ "\n");
+            break;
+        case 0:
+            Environment.Exit(0);
+            break;
+    }
+}
 
-// Write program to enter an integer number of centuries and convert it to years, days, hours,
-// minutes, seconds, milliseconds, microseconds, nanoseconds. Use an appropriate data
-// type for every data conversion. Beware of overflows!
 
-Console.WriteLine("Please enter a number of centuries:");
-int centuries = int.Parse(Console.ReadLine());
-// Call calCenturies() to do calculation from UnderstandingType class
-UnderstandingType.calCenturies(centuries);
-Console.WriteLine(new string('*', 100)+ "\n");
+
+

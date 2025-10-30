94fb44c41b1d2ae1ef80b9609d6a3ec6195fdceb
Leapheng Keam <leaphengkeam@gmail.com>
2025-10-30 12:02:59 -0400

    finished fibonacci sequence

--- Assignment03/program.cs
+++ Assignment03/program.cs
@@ -4,8 +4,9 @@ using System.Runtime.InteropServices;
 
 Console.WriteLine("\t***********ASSIGNMENT 03***********\n");
 
+// Array reverse
+Console.WriteLine("\t*****Reverse Array*****");
 int n;
-
 Console.Write("Enter the length of the numbers: ");
 n = int.Parse(Console.ReadLine());
 Console.WriteLine();
@@ -13,10 +14,23 @@ int [] numbers = GenerateNumbers(n, 1, 100);
 
 Console.Write("Array before: ");
 PrintNumbers(numbers);
-Console.WriteLine(new string('*', 100));
+Console.WriteLine(new string('*', 50));
 Console.Write("Array after:  ");
 Reverse(numbers, n);
 PrintNumbers(numbers);
+Console.WriteLine(new string('*', 50));
+
+// Fibonacci Sequence
+Console.WriteLine("\n\t*****Fibonacci Sequence*****");
+Console.WriteLine("\nFirst 10 Fibonacci numbers:");
+for (int i = 1; i <= 10; i++)
+{
+    Console.Write(Fibonacci(i) + " ");
+}
+Console.WriteLine();
+
+// OOP
+Console.WriteLine("\n\t*****Object-Oriented Programming*****");
 // Create a method to generate number based on user input
 static int[] GenerateNumbers(int size, int min, int max)
 {
@@ -48,5 +62,18 @@ static void Reverse(int[] numbers, int length)
         int temp = numbers[i];
         numbers[i] = numbers[length - i - 1];
         numbers[length - i - 1] = temp;
+    }
+}
+
+// Create a method to return Fibonacci number
+static int Fibonacci(int n)
+{
+    if (n == 1 || n == 2)
+    {
+        return 1;
+    }
+    else
+    {
+        return Fibonacci(n - 1) + Fibonacci(n - 2);
     }
 }

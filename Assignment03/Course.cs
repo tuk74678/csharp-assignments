1a1226630a1d588e02197502657c37ed2470c015
Leapheng Keam <leaphengkeam@gmail.com>
2025-10-30 23:02:14 -0400

    initialized properties for course class

--- Assignment03/Course.cs
+++ Assignment03/Course.cs
@@ -1,6 +1,8 @@
 ﻿namespace Assignment03;
 
-public class Course
+public class Course: ICourseService
 {
-    
+    public string Title { get; set; }
+    private Student[] _enrolledStudents = new Student[10];
+    private int _studentCount = 0;
 }

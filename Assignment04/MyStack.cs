cafec689a1c6f214896291e020f25ae5e6bfa0a5
Leapheng Keam <leaphengkeam@gmail.com>
2025-10-31 18:29:02 -0400

    declared all methods to perform stack operations

--- Assignment04/MyStack.cs
+++ Assignment04/MyStack.cs
@@ -1,6 +1,33 @@
 ﻿namespace Assignment04;
 
-public class MyStack
+public class MyStack<T>
 {
-   
+    private T[] stack;          // create an array of stack
+    private int top = 0;        // index that points to the top of the element
+
+    public MyStack()
+    {
+        
+    }
+    
+    // create method to count the total size
+    public int count()
+    {
+        return stack.Length;
+    }
+    
+    // Push into the stack
+    public void Push(T value)
+    {
+        
+    }
+    
+    // Remove the top from stack
+    public T Pop()
+    {
+        return stack[top];
+    }
+    
+    
+    
 }

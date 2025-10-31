7720f9c6bde0d8856207b3c12f0a7899a66db08b
Leapheng Keam <leaphengkeam@gmail.com>
2025-10-31 19:27:01 -0400

    declared all methods in MyList class

--- Assignment04/MyList.cs
+++ Assignment04/MyList.cs
@@ -2,5 +2,45 @@
 
 public class MyList<T>
 {
+    // Constructor
+    public MyList()
+    {
+        
+    }
+
+    public void Add(T element)
+    {
+        
+    }
+
+    public T Remove(int index)
+    {
+        return;
+    }
+
+    public bool Contains(T element)
+    {
+        return true;
+    }
+
+    public void Clear()
+    {
+        
+    }
+
+    public void InsertAt(T element, int index)
+    {
+        
+    }
+
+    public void DeleteAt(int index)
+    {
+        
+    }
+
+    public T Find(int index)
+    {
+        
+    }
     
 }

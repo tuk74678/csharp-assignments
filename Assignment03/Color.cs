78104c86417faa46da1f03aab5492ef3b79effa4
Leapheng Keam <leaphengkeam@gmail.com>
2025-10-30 23:42:27 -0400

    List all properties and create a constructor

--- Assignment03/Color.cs
+++ Assignment03/Color.cs
@@ -2,5 +2,18 @@
 
 public class Color
 {
+    // List all properties
+    public int Red { get; private set; }
+    public int Green { get; private set; }
+    public int Blue { get; private set; }
+    public int Alpha { get; private set; } 
     
+    // Constructor
+    public Color(int red, int green, int blue, int alpha = 255)
+    {
+        Red = red;
+        Green = green;
+        Blue = blue;
+        Alpha = alpha;
+    }
 }
